62a7950ee9776e6893fe7ca304cdea4aa895fd65
01Joseph-Hwang10 <joseph95501@gmail.com>
2021-03-02 16:04:44 +0900

    api done, needa do E2E testing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true, // Block connection of forbidden stuff
+      transform: true, // If there's request, Nest JS does all the work
+    })
+  )
   await app.listen(3001);
 }
 bootstrap();
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,30 +1,44 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateMovieDTO } from './dto/create-movie.dto';
+import { UpdateMovieDTO } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
 
+    constructor(private readonly moviesService: MoviesService) {}
+
+
     @Get()
-    getAll() {
-        return "This will erturn all movies";
+    getAll(): Movie[] {
+        return this.moviesService.getAll();
     }
 
+    @Get("/search")
+    search(@Query('year') searchingYear: string){
+        return `We are searching for a movie made after: ${searchingYear}`
+    }
+
+    // Put Id at the bottom!
     @Get('/:id')
-    getOne(@Param("id") movieId: string){
-        return `This will return one movie with the id: ${movieId}`;
+    getOne(@Param("id") movieId: number): Movie {
+        return this.moviesService.getOne(movieId)
     }
 
     @Post()
-    create(){
-        return "This will create a movie";
+    create(@Body() movieData: CreateMovieDTO){
+        return this.moviesService.create(movieData)
     }
 
     @Delete("/:id")
-    remove(@Param('id') movieId:string){
-        return `This will delete a movie with the id: ${movieId}`
+    remove(@Param('id') movieId:number){
+        return this.moviesService.deleteOne(movieId)
     }
 
     @Patch('/:id')
-    patch(@Param('id') movieId:string){
-        return `This will patch a movie with the id: ${movieId}`
+    patch(@Param('id') movieId:number, @Body() updateData: UpdateMovieDTO){
+        return this.moviesService.update(movieId, updateData)
     }
+
 }
